e200a66d4d4aa584ed5710b96e3bfd7a8c176f0c
Daichi Hayashi <daichikuwa0618@gmail.com>
2022-02-10 17:19:16 +0900

    use parent vc

--- TabmanPlayground/SceneDelegate.swift
+++ TabmanPlayground/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let scene = (scene as? UIWindowScene) else { return }
 
-        let vc = ViewController()
+        let vc = ParentViewController()
         vc.title = "Tabman Playground"
         let navigationController = UINavigationController(rootViewController: vc)
 
